34a8844ac1b20e0ea8320fe8320a0cd8f893ea36
James Ying <jyab0617@gmail.com>
2022-09-14 10:29:08 -0400

    add paddle movement and constraints functionality

--- pong.py
+++ pong.py
@@ -13,6 +13,7 @@ PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
 
 class Paddle:
     COLOR = WHITE
+    VEL = 4
 
     def __init__(self, x, y, width, height):
         self.x = x
@@ -23,6 +24,15 @@ class Paddle:
     def draw(self, win):
         pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
 
+    def move(self, up=True):
+        if (up):
+            self.y -= self.VEL
+        else: 
+            self.y += self.VEL
+
+            
+
+
 def draw(window, paddles):
     window.fill(BLACK)
 
@@ -31,6 +41,16 @@ def draw(window, paddles):
 
     pygame.display.update()
 
+def handle_paddle_movement(keys, left_paddle, right_paddle):
+    if keys[pygame.K_w] and (left_paddle.y - left_paddle.VEL >= 0):
+        left_paddle.move(up=True)
+    if keys[pygame.K_s] and (left_paddle.y + left_paddle.VEL + left_paddle.height <= HEIGHT):
+        left_paddle.move(up=False)
+
+    if keys[pygame.K_UP] and (right_paddle.y - right_paddle.VEL >= 0):
+        right_paddle.move(up=True)
+    if keys[pygame.K_DOWN] and (right_paddle.y + right_paddle.VEL +right_paddle.height <= HEIGHT):
+        right_paddle.move(up=False)
 
 def main():
     run = True
@@ -43,14 +63,17 @@ def main():
     while run:
         # regulate game to 60fps
         clock.tick(FPS)
+        
         draw(WINDOW, [left_paddle, right_paddle]) 
 
         for event in pygame.event.get():
-
             # when player clicks close window btn
             if event.type == pygame.QUIT:
                 run = False
                 break
+        
+        keys = pygame.key.get_pressed()
+        handle_paddle_movement(keys, left_paddle, right_paddle)
 
     pygame.quit()
 
