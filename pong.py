8dc78dc20f2a8becba94a49b16689b04d0794245
James Ying <jyab0617@gmail.com>
2022-09-15 22:46:02 -0400

    add collision and bounce angle function to ball

--- pong.py
+++ pong.py
@@ -65,15 +65,48 @@ def draw(window, paddles, ball):
     ball.draw(WINDOW)
     pygame.display.update()
 
+def handle_collision(ball, left_paddle, right_paddle):
+
+    # ball will bounce back at opposite direction touching top and bot of screen
+    if (ball.y + ball.radius >= HEIGHT):
+        ball.y_vel *= -1
+    elif (ball.y - ball.radius <= 0):
+        ball.y_vel *= -1
+
+    # hitting the left paddle
+    if (ball.x_vel < 0):
+        if (ball.y >= left_paddle.y and ball.y <= left_paddle.y + left_paddle.height):
+            if ((ball.x - ball.radius) <= (left_paddle.x + left_paddle.width)):
+                ball.x_vel *= -1
+
+                middle_y = left_paddle.y + left_paddle.height / 2
+                difference_in_y = middle_y - ball.y
+                reduction_factor = (left_paddle.height / 2) / ball.MAX_VEL 
+                y_vel = difference_in_y / reduction_factor
+                ball.y_vel = y_vel * -1
+                
+    else:
+    # hitting the right paddle
+        if (ball.x_vel > 0):
+            if (ball.y >= right_paddle.y and ball.y <= right_paddle.y + right_paddle.height):
+                if ((ball.x + ball.radius) >= right_paddle.x):
+                    ball.x_vel *= -1
+
+                    middle_y = right_paddle.y + right_paddle.height / 2
+                    difference_in_y = middle_y - ball.y
+                    reduction_factor = (right_paddle.height / 2) / ball.MAX_VEL 
+                    y_vel = difference_in_y / reduction_factor
+                    ball.y_vel = y_vel * -1
+
 def handle_paddle_movement(keys, left_paddle, right_paddle):
-    if keys[pygame.K_w] and (left_paddle.y - left_paddle.VEL >= 0):
+    if (keys[pygame.K_w] and (left_paddle.y - left_paddle.VEL >= 0)):
         left_paddle.move(up=True)
-    if keys[pygame.K_s] and (left_paddle.y + left_paddle.VEL + left_paddle.height <= HEIGHT):
+    if (keys[pygame.K_s] and (left_paddle.y + left_paddle.VEL + left_paddle.height <= HEIGHT)):
         left_paddle.move(up=False)
 
-    if keys[pygame.K_UP] and (right_paddle.y - right_paddle.VEL >= 0):
+    if (keys[pygame.K_UP] and (right_paddle.y - right_paddle.VEL >= 0)):
         right_paddle.move(up=True)
-    if keys[pygame.K_DOWN] and (right_paddle.y + right_paddle.VEL +right_paddle.height <= HEIGHT):
+    if (keys[pygame.K_DOWN] and (right_paddle.y + right_paddle.VEL +right_paddle.height <= HEIGHT)):
         right_paddle.move(up=False)
 
 def main():
@@ -99,7 +132,7 @@ def main():
         
         keys = pygame.key.get_pressed()
         handle_paddle_movement(keys, left_paddle, right_paddle)
-
+        handle_collision(ball, left_paddle, right_paddle)
         ball.move()
 
     pygame.quit()
